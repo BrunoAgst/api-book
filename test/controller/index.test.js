ccec1d309e9ed9e906ae1605b6f0093b986333b4
BrunoAgst <bruno_agst@hotmail.com>
2022-05-24 23:32:16 -0300

    prepare struture to create tests controller

---
+++ test/controller/index.test.js
@@ -0,0 +1,29 @@
+const { describe, test, jest: _jest, beforeEach, beforeAll, expect } = require('@jest/globals')
+
+describe('#Controller', () => {
+    describe('bookCreate', () => {
+        beforeEach(() => {
+            _jest.clearAllMocks()
+        })
+    })
+    describe('bookGetAll', () => {
+        beforeEach(() => {
+            _jest.clearAllMocks()
+        })
+    })
+    describe('bookGetDetails', () => {
+        beforeEach(() => {
+            _jest.clearAllMocks()
+        })
+    })
+    describe('bookPatch', () => {
+        beforeEach(() => {
+            _jest.clearAllMocks()
+        })
+    })
+    describe('bookDelete', () => {
+        beforeEach(() => {
+            _jest.clearAllMocks()
+        })
+    })
+})
