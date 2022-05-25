59d13d2ad8f1ac53fc7a61e24ed7bdeef9fbe35d
BrunoAgst <bruno_agst@hotmail.com>
2022-05-24 22:50:03 -0300

    add createBook test in new describe

--- test/service/repository/bookRepository.test.js
+++ test/service/repository/bookRepository.test.js
@@ -4,75 +4,78 @@ const BookSchema = require('../../../src/schema/Book')
 
 describe('#BookRepository', () => {
 
-    beforeEach(() => {
-
-        _jest.clearAllMocks()
-        _jest.spyOn(
-            BookSchema,
-            'findOne'
-        ).mockResolvedValue()
-
-        _jest.spyOn(
-            BookSchema,
-            'create'
-        ).mockResolvedValue({})
-
-    })
-
-    beforeAll(() => _jest.clearAllMocks())
-
-    test('should create a new book', async () => {
-        const payload = {
-            "sbn": "1236565udafadd444d",
-            "name": "mario bros volume 3",
-            "description": "game",
-            "author": "nintendo",
-            "inventory": 10
-        }
-
-        const bookRepository = new BookRepository()
-        const result = await bookRepository.createBook(payload)
-        expect(result).toEqual('ok')
-    })
-
-    test('should create error because book exists', async () => {
+    describe('#createBook', () => {
+        beforeEach(() => {
     
-        _jest.spyOn(
-            BookSchema,
-            'findOne'
-        ).mockResolvedValue({})
-
-        const payload = {
-            "sbn": "1236565udafadd444d",
-            "name": "mario bros volume 3",
-            "description": "game",
-            "author": "nintendo",
-            "inventory": 10
-        }
-
-        const bookRepository = new BookRepository()
-        const result = await bookRepository.createBook(payload)
-        expect(result).toEqual({ status: 401, error: 'Book exists' })
-    })
-
-    test('should create return error', async () => {
+            _jest.clearAllMocks()
+            _jest.spyOn(
+                BookSchema,
+                'findOne'
+            ).mockResolvedValue()
     
-        _jest.spyOn(
-            BookSchema,
-            'findOne'
-        ).mockImplementation(() => { throw new Error('error') })
-
-        const payload = {
-            "sbn": "1236565udafadd444d",
-            "name": "mario bros volume 3",
-            "description": "game",
-            "author": "nintendo",
-            "inventory": 10
-        }
-
-        const bookRepository = new BookRepository()
-        const result = await bookRepository.createBook(payload)
-
-        expect(result).toBeTruthy()
+            _jest.spyOn(
+                BookSchema,
+                'create'
+            ).mockResolvedValue({})
+    
+        })
+    
+        beforeAll(() => _jest.clearAllMocks())
+    
+        test('should create a new book', async () => {
+            const payload = {
+                "sbn": "1236565udafadd444d",
+                "name": "mario bros volume 3",
+                "description": "game",
+                "author": "nintendo",
+                "inventory": 10
+            }
+    
+            const bookRepository = new BookRepository()
+            const result = await bookRepository.createBook(payload)
+            expect(result).toEqual('ok')
+        })
+    
+        test('should create error because book exists', async () => {
+        
+            _jest.spyOn(
+                BookSchema,
+                'findOne'
+            ).mockResolvedValue({})
+    
+            const payload = {
+                "sbn": "1236565udafadd444d",
+                "name": "mario bros volume 3",
+                "description": "game",
+                "author": "nintendo",
+                "inventory": 10
+            }
+    
+            const bookRepository = new BookRepository()
+            const result = await bookRepository.createBook(payload)
+            expect(result).toEqual({ status: 401, error: 'Book exists' })
+        })
+    
+        test('should create return error', async () => {
+        
+            _jest.spyOn(
+                BookSchema,
+                'findOne'
+            ).mockImplementation(() => { throw new Error('error') })
+    
+            const payload = {
+                "sbn": "1236565udafadd444d",
+                "name": "mario bros volume 3",
+                "description": "game",
+                "author": "nintendo",
+                "inventory": 10
+            }
+    
+            const bookRepository = new BookRepository()
+            const result = await bookRepository.createBook(payload)
+    
+            expect(result).toBeTruthy()
+        })
     })
+
 })
