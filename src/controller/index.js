00679ed2d7f9201bfabbdee8a9b082a812858a05
BrunoAgst <bruno_agst@hotmail.com>
2022-05-25 11:34:38 -0300

    created bookCreate test

--- src/controller/index.js
+++ src/controller/index.js
@@ -21,7 +21,8 @@ module.exports = {
 
         } catch (error) {
             console.log(error)
-            response.status(500).json({ error: "Internal Server Error" })
+            response.status(500)
+            response.json({ error: "Internal Server Error" })
         }
     },
 
--- test/controller/index.test.js
+++ test/controller/index.test.js
@@ -1,29 +1,67 @@
 const { describe, test, jest: _jest, beforeEach, beforeAll, expect } = require('@jest/globals')
-
+const Controller = require('../../src/controller/index')
+const BookRepository = require('../../src/service/repository/bookRepository')
 describe('#Controller', () => {
     describe('bookCreate', () => {
         beforeEach(() => {
             _jest.clearAllMocks()
         })
-    })
-    describe('bookGetAll', () => {
-        beforeEach(() => {
-            _jest.clearAllMocks()
+
+        test('should return book successfully created', async () => {
+            const request = {
+                body: {
+                    "sbn": "1236565udafadd444d",
+                    "name": "mario bros volume 3",
+                    "description": "game",
+                    "author": "nintendo",
+                    "inventory": 10
+                }
+            }
+
+            const response = {
+                status: _jest.fn(),
+                json: _jest.fn()
+            }
+
+
+            _jest.spyOn(
+                BookRepository.prototype,
+                'createBook'
+            ).mockResolvedValue([])
+            
+
+            const result = await Controller.bookCreate(request, response)
+            expect(response.json).toHaveBeenCalled()
+            expect(result).toBeUndefined()
         })
-    })
-    describe('bookGetDetails', () => {
-        beforeEach(() => {
-            _jest.clearAllMocks()
-        })
-    })
-    describe('bookPatch', () => {
-        beforeEach(() => {
-            _jest.clearAllMocks()
-        })
-    })
-    describe('bookDelete', () => {
-        beforeEach(() => {
-            _jest.clearAllMocks()
+
+        test('should return book return error', async () => {
+            const request = {
+                body: {
+                    "sbn": "1236565udafadd444d",
+                    "name": "mario bros volume 3",
+                    "description": "game",
+                    "author": "nintendo",
+                    "inventory": 10
+                }
+            }
+
+            const response = {
+                status: _jest.fn(),
+                json: _jest.fn()
+            }
+
+
+            _jest.spyOn(
+                BookRepository.prototype,
+                'createBook'
+            ).mockReturnValue({ error: true, status: '401'})
+            
+
+            const result = await Controller.bookCreate(request, response)
+            expect(response.status).toHaveBeenCalled()
+            expect(response.json).toHaveBeenCalled()
+            expect(result).toBeUndefined()
         })
     })
 })
